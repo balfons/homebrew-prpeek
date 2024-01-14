40c1b1f2e7e6785a21a3b1f4eb236df39ff302e8
Alfons Bengtegård <alfons.bengtegard@astrazeneca.com>
2024-01-14 17:15:42 +0100

    Fix release 1.0.4

--- Formula/prpeek.rb
+++ Formula/prpeek.rb
@@ -3,8 +3,8 @@
 class Prpeek < Formula
   desc "Show status of relevant pull requests live. - CLI"
   homepage "https://github.com/balfons/prpeek"
-  url "https://github.com/balfons/prpeek/archive/refs/tags/1.0.4.tar.gz"
-  sha256 "3fae161b2710318c73e1b88707e83845905432232357c782b6c03ded3527002f"
+  url "https://github.com/balfons/prpeek/releases/download/1.0.4/prpeek.tar.gz"
+  sha256 "1de6ae54166dbd3510c156b11eeab20fe85f069bfdf65252c5401ffb64ae3414"
   license "MIT"
   version "1.0.4"
 
